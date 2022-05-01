47b85614487f6ff16544c80a13eed7ef977f4c96
Derek Johnson <derekmj74@gmail.com>
2022-05-01 22:34:05 +0100

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Adapted from https://adactio.com/serviceworker.js
 'use strict';
 
-const version = '2022-05-01b';
+const version = '2022-05-01c';
 const cacheName = `cache-${version}`;
 
 function updateCache() {
