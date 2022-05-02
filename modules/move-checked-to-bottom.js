bb4c84cf72357d5bd44bbde986e582b79e56585f
Derek Johnson <derekmj74@gmail.com>
2022-05-02 23:31:10 +0100

    Working everywhere except Safari

---
+++ modules/move-checked-to-bottom.js
@@ -0,0 +1,16 @@
+// Remove checked items and append them to the bottom of the list
+export default (list) => {
+  const items = list.querySelectorAll('li');
+  const itemsArray = Array.from(items);
+
+  const ordered = itemsArray.sort((a, b) => {
+    return a.querySelector('input').checked > b.querySelector('input').checked ? 1 : -1;
+  });
+
+  const fragment = new DocumentFragment();
+  ordered.forEach(item => {
+    fragment.appendChild(item);
+  });
+
+  return fragment;
+}
